4b8f6c3cf3b8beafc771b5ed956d1a5058c14585
Mikkel Nielsen <mikkel1510@live.dk>
2025-11-28 12:02:40 +0100

    Fixed app and dockerfile ports

--- backend/app.py
+++ backend/app.py
@@ -145,4 +145,4 @@ def save_question(db: sqlalchemy.engine.base.Engine, question: str, answer1: str
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
